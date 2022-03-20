2c4f7abc8231f329c44bf2635335ac38f19705bd
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-03-20 22:41:00 +0100

    feat(async): add serialized async functions
    This allows to serialize multiple calls to the same async function
    instead of running them in parallel which can help to prevent race
    conditions.

--- lua/perapera/async.lua
+++ lua/perapera/async.lua
@@ -3,7 +3,7 @@ Turn callback pattern to coroutine pattern, based on
 https://luyuhuang.tech/2020/09/13/callback-to-coroutine.html
 --]]
 
-local async = {}
+local async = {_fifo = {}}
 
 -- Suspend a callback-style function (i.e. yield a coroutine and resume it in
 -- its callback). Useful to use callback pattern instead of coroutine pattern.
@@ -14,26 +14,55 @@ function async.suspend(func, ...)
   end)
 end
 
--- Run an async function (i.e. a function which contains a wrapped
--- callback-style function somewhere in its call stack).
-function async.run(f, ...)
+local function callback_create(f)
   local co, exec = coroutine.create(f)
+
   exec = vim.schedule_wrap(function(...)
     local ok, data = coroutine.resume(co, ...)
     if not ok then
       error(debug.traceback(co, data))
     end
     if coroutine.status(co) ~= "dead" then
-      data(exec)
+        data(exec)
+    elseif async._fifo[f] then
+      if #async._fifo[f] > 0 then
+        table.remove(async._fifo[f], 1)()
+      else
+        async._fifo[f] = nil
+      end
     end
   end)
-  exec(...)
+
+  return exec
+end
+
+-- Run an async function (i.e. a function which contains a wrapped
+-- callback-style function somewhere in its call stack).
+function async.run(f, ...)
+  callback_create(f)(...)
+end
+
+function async.serial_run(f, ...)
+  local exec, args = callback_create(f), {...}
+  if async._fifo[f] then
+    table.insert(async._fifo[f], function() exec(unpack(args)) end)
+  else
+    async._fifo[f] = {}
+    exec(...)
+  end
 end
 
 function async.wrap(f, ...)
   local args = {...}
   return function(...)
     async.run(f, unpack(vim.list_extend(vim.list_slice(args), {...})))
+  end
+end
+
+function async.serial_wrap(f, ...)
+  local args = {...}
+  return function(...)
+    async.serial_run(f, unpack(vim.list_extend(vim.list_slice(args), {...})))
   end
 end
 
