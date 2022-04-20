01c7b5c29de648a1a5da7352c5b38db206aff56f
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-04-20 12:35:18 +0200

    feat(utils): backport nvim_buf_get_text
    nvim_buf_get_text is only available in Neovim >= 0.7. Since we target
    Neovim >= 0.6 as of now, we have to implement it ourselves.

---
+++ lua/perapera/utils/api.lua
@@ -0,0 +1,18 @@
+local api = {}
+
+-- Implement nvim_buf_get_text, which is not available in neovim <= 0.6.*
+--
+--  Gets a range from the buffer.
+--  This differs from |nvim_buf_get_lines()| in that it allows
+--  retrieving only portions of a line.
+--  Indexing is zero-based. Column indices are end-exclusive.
+--  Prefer |nvim_buf_get_lines()| when retrieving entire lines.
+function api.nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col)
+  local input = vim.api.nvim_buf_get_lines(buffer, start_row, end_row, true)
+  if #input > 0 then
+    input[1], input[#input] = input[1]:sub(start_col + 1), input[#input]:sub(1, end_col)
+  end
+  return input
+end
+
+return api
--- lua/perapera/utils/init.lua
+++ lua/perapera/utils/init.lua
@@ -2,6 +2,7 @@ local utils = {
   buffer = require("perapera.utils.buffer"),
   table = require("perapera.utils.table"),
   properties = require("perapera.utils.properties"),
+  api = require("perapera.utils.api")
 }
 
 return utils
