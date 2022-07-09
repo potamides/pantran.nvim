3bcc05aa39bbd9038158ea627f21544b41a2ba60
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-07-09 11:36:54 +0200

    test(ui): add new test cases

--- tests/ui_spec.lua
+++ tests/ui_spec.lua
@@ -4,24 +4,48 @@ local async = require("pantran.async")
 
 local example = "Hello World!"
 
-local co = async.run(describe, "ui", function()
-    it('can be started', function()
-      local coords = {srow = -1, scol = -1, erow = -1, ecol = -1}
-      assert.has_not.errors(function() ui = ui.new(engines.default, nil, nil, coords, example) end)
-    end)
+describe("ui", function()
+  async.run(it, 'can be started', function()
+    local coords = {srow = -1, scol = -1, erow = -1, ecol = -1}
+    assert.has_not.errors(function() ui = ui.new(engines.default, nil, nil, coords, example) end)
+    assert.are.equal(vim.fn.exists(":Pantran"), 2)
+  end)
 
-    it('sets source text', function()
-      assert.are.equal(ui.input, example)
-    end)
+  local co = async.run(it, 'can be locked', function()
+    assert.are_not.equal(ui._mutex._owner, coroutine.running())
+    ui:lock()
+    assert.are.equal(ui._mutex.active, true)
+    assert.are.equal(ui._mutex._owner, coroutine.running())
+    ui:unlock()
+  end)
+  async.join(co)
 
-    it('uses correct languages', function()
-      assert.are.equal(ui.source, engines.default.config.default_source)
-      assert.are.equal(ui.target, engines.default.config.default_target)
-    end)
+  co = async.run(it, 'sets text fields correctly', function()
+    ui:lock()
+    assert.are.equal(ui.input, example)
+    assert.are_not.equal(ui.translation, "")
+    ui:unlock()
+  end)
+  async.join(co)
 
-    it('uses engine name as title', function()
-      assert.are.equal(ui._win.languagebar._title, engines.default.name)
-    end)
+  it('uses correct languages', function()
+    assert.are.equal(ui.source, engines.default.config.default_source)
+    assert.are.equal(ui.target, engines.default.config.default_target)
+  end)
+
+  it('uses engine name as title', function()
+    assert.are.equal(ui._win.languagebar._title, engines.default.name)
+  end)
+
+  it('closes when leaving buffer', function()
+    vim.cmd("wincmd p")
+    for _, win in pairs(ui._win) do
+      assert.is_true(win.closed)
+    end
+    assert.are.equal(#vim.api.nvim_list_bufs(), 1)
+    assert.are.equal(#vim.api.nvim_list_wins(), 1)
+    collectgarbage()
+    -- since current coroutine is still running this is 1 and not 0
+    assert.are.equal(vim.tbl_count(async.mutex._owned), 1)
+  end)
 end)
-
-async.join(co)
