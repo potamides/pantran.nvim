26e407fc1a0cb52227cf8f9f052a24ecc0c050c4
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-07-22 10:09:05 +0200

    test(engines): add test case which doesn't require setup
    Tests the bare minimum of methods engines are required to implement.
    This is useful for engines which can't be tested using github actions as
    the necessary setup steps can't get executed.

--- tests/engines_spec.lua
+++ tests/engines_spec.lua
@@ -29,42 +29,52 @@ local engines = {
 
 local example = "Hello World!"
 
--- caveat: with github actions this only tests engines which do not require
--- additional setup steps. On the bright side these are the most likely to
--- fail.
 for engine, dotest in pairs(engines) do
-  if dotest and pcall(engine.setup) then
+  if dotest then
     local co = async.run(describe, engine.name, function()
-        it('supports multiple languages', function()
-          local languages = engine.languages()
-          assert.are_not.equal(vim.tbl_count(languages.source), 0)
-          assert.are_not.equal(vim.tbl_count(languages.target), 0)
+        it('implements all required functions', function()
+            assert.is_function(engine.setup)
+            assert.is_function(engine.languages)
+            assert.is_function(engine.switch)
+            assert.is_function(engine.detect)
+            assert.is_function(engine.translate)
         end)
 
-        it('can switch languages', function()
-          local runs, maxruns = 0, 100
-          for src, _ in pairs(engine.languages().source) do
-            for tgt, _ in pairs(engine.languages().target) do
-              local new_src, new_tgt = engine.switch(src, tgt)
-              assert.is_true(new_src == vim.NIL or type(new_src) == "string")
-              assert.is_true(type(new_tgt) == "string")
-              runs = runs + 1
-              if runs > maxruns then
-                return
+        -- caveat: with github actions this only tests engines which do not require
+        -- additional setup steps. On the bright side these are the most likely to
+        -- fail.
+        if pcall(engine.setup) then
+          it('supports multiple languages', function()
+            local languages = engine.languages()
+            assert.are_not.equal(vim.tbl_count(languages.source), 0)
+            assert.are_not.equal(vim.tbl_count(languages.target), 0)
+          end)
+
+          it('can switch languages', function()
+            local runs, maxruns = 0, 100
+            for src, _ in pairs(engine.languages().source) do
+              for tgt, _ in pairs(engine.languages().target) do
+                local new_src, new_tgt = engine.switch(src, tgt)
+                assert.is_true(new_src == vim.NIL or type(new_src) == "string")
+                assert.is_true(type(new_tgt) == "string")
+                runs = runs + 1
+                if runs > maxruns then
+                  return
+                end
               end
             end
-          end
-        end)
+          end)
 
-        it('can detect languages', function()
-          local detected = engine.detect(example)
-          assert.is_true(vim.tbl_contains(vim.tbl_keys(engine.languages().source), detected))
-        end)
+          it('can detect languages', function()
+            local detected = engine.detect(example)
+            assert.is_true(vim.tbl_contains(vim.tbl_keys(engine.languages().source), detected))
+          end)
 
-        it('can translate', function()
-          local translation = engine.translate(example)
-          assert.are_not.equal(translation, "") -- it should return something
-        end)
+          it('can translate', function()
+            local translation = engine.translate(example)
+            assert.are_not.equal(translation, "") -- it should return something
+          end)
+        end
     end)
 
     async.join(co)
